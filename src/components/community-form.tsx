8780b9db53a837edbab0a1d8cdd5673c242e4523
Riky Raj <rajriky28@gmail.com>
2025-12-26 10:46:43 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/components/community-form.tsx
+++ src/components/community-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
-import { useEffect, useRef } from "react";
+import { useActionState, useEffect, useRef } from "react";
+import { useFormStatus } from "react-dom";
 import { subscribeToUpdates } from "@/app/actions";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -19,7 +19,7 @@ function SubmitButton() {
 }
 
 export function CommunityForm() {
-  const [state, formAction] = useFormState(subscribeToUpdates, {
+  const [state, formAction] = useActionState(subscribeToUpdates, {
     message: "",
     success: false,
   });
