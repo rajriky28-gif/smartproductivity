188073050a4159602ef9f0b4ce8b2c6ea3e46c60
Riky Raj <rajriky28@gmail.com>
2025-12-29 09:32:55 +0000

    This error is happening because your **Privacy Policy** page is trying t

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,4 +1,6 @@
 
+export const dynamic = 'force-dynamic'
+
 import { Suspense } from 'react';
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
