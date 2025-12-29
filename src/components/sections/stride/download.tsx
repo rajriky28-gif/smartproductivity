2250330c5b37b2f70b071dedf901d690fa4839ab
Riky Raj <rajriky28@gmail.com>
2025-12-29 08:57:55 +0000

    in start using stride show stride logo in mobile and background white

--- src/components/sections/stride/download.tsx
+++ src/components/sections/stride/download.tsx
@@ -1,3 +1,4 @@
+
 import { FadeIn } from '@/components/fade-in';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -61,10 +62,8 @@ export function DownloadSection() {
               <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
               <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
               <div className="h-[64px] w-[3px] bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
-              <div className="rounded-[2rem] overflow-hidden w-full h-full bg-white dark:bg-zinc-800 flex items-center justify-center">
-                 <p className="text-muted-foreground italic text-center text-sm">
-                    [Minimal Phone Mockup showing Quick Add]
-                </p>
+              <div className="rounded-[2rem] overflow-hidden w-full h-full bg-white dark:bg-white flex items-center justify-center">
+                 <Image src="/stridelogo.png" alt="Stride Logo" width={128} height={128} className="border rounded-2xl" />
               </div>
             </div>
           </FadeIn>
